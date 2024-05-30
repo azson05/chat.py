814d5449905ae0d9442800a3f483f00347fa77a9
azson05 <songywkd@gmail.com>
2024-05-30 15:50:08 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from openai import OpenAI
-client = OpenAI(api_key="")
-assistant = client.beta.assistants.create(
-  instructions="my chatbot",
-  model="gpt-4o"
-)
+st.header("OpenAI API Key를 적어주세요.")
+api = st.text_input("API Key?")
 
+if st.button("확인"):
+  client = OpenAI(api_key=f"{api}")
+  assistant = client.beta.assistants.create(
+    instructions = "You are a helpful assistant.",
+    model='gpt-3.5-turbo'
+  )
+st.divider()
 st.image("https://docs.streamlit.io/logo.svg")
 
 st.header("무엇이든 물어보세요.")
