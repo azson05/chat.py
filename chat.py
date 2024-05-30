a38fb5b4ba842f2f2f61972178347c39a695c038
azson05 <songywkd@gmail.com>
2024-05-30 16:19:15 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -12,18 +12,7 @@ if st.button("확인"):
     instructions = "You are a helpful assistant.",
     model='gpt-4o'
   )
-  thread = client.beta.threads.create(
-    messages=[
-      {
-          "role":"user",
-          "content": "부산의 위치를 알려줘"
-      }
-    ]
-  )
-  run = client.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id
-  )
+  
 st.divider()
 
 st.header("무엇이든 물어보세요.")
@@ -33,19 +22,15 @@ if st.button("실행하기"):
   thread = client.beta.threads.create(
     messages=[
       {
-        "role": "user",
-        "content": f"{prompt}",
+          "role":"user",
+          "content": f"{prompt}"
       }
     ]
   )
-  run = client.beta.threads.runs.create_and_poll(
-      thread_id=thread.id,
-      assistant_id=assistant.id
+  run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
   )
-  run
-  thread_messages = client.beta.threads.messages.list(thread.id, run_id=run.id)
-  for msg in thread_messages.data:
-    print(f"{msg.role}: {msg.content[0].text.value}")
   st.markdown(f"질문: {prompt}")
 
 st.divider()
