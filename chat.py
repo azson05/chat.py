280b70cd84b35fa07c0dc4cb315cfb67fb16e938
azson05 <songywkd@gmail.com>
2024-05-30 15:38:01 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,7 +5,6 @@ st.header("OpenAI API Key를 적어주세요.")
 api = st.text_input("API Key?")
 
 if st.button("확인"):
-  from openai import OpenAI
   client = OpenAI(api_key=f"{api}")
   assistant = client.beta.assistants.create(
     instructions = "You are a helpful assistant.",
