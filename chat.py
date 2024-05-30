506e6668a4eb00a1c9f90de6ebdf08098e362b1c
azson05 <songywkd@gmail.com>
2024-05-30 15:55:42 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from openai import OpenAI
+
+st.image("https://docs.streamlit.io/logo.svg")
+
 st.header("OpenAI API Key를 적어주세요.")
 api = st.text_input("API Key?")
 
@@ -7,15 +10,30 @@ if st.button("확인"):
   client = OpenAI(api_key=f"{api}")
   assistant = client.beta.assistants.create(
     instructions = "You are a helpful assistant.",
-    model='gpt-3.5-turbo'
+    model='gpt-4o'
   )
 st.divider()
-st.image("https://docs.streamlit.io/logo.svg")
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
 
 if st.button("실행하기"):
+  thread = client.beta.threads.create(
+    messages=[
+      {
+        "role": "user",
+        "content": f"{prompt}",
+      }
+    ]
+  )
+  run = client.beta.threads.runs.create_and_poll(
+      thread_id=thread.id,
+      assistant_id=assistant.id
+  )
+  run
+  thread_messages = client.beta.threads.messages.list(thread.id, run_id=run.id)
+  for msg in thread_messages.data:
+    print(f"{msg.role}: {msg.content[0].text.value}")
   st.markdown(f"질문: {prompt}")
 
 st.divider()
