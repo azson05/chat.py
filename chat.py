527141249e90239774e8c704e61c488d63b50a3d
azson05 <songywkd@gmail.com>
2024-05-30 16:40:16 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,13 +3,18 @@ import openai
 
 st.image("https://docs.streamlit.io/logo.svg")
 
+st.header("OpenAI API Key를 적어주세요.") 
+api = st.text_input("API Key?")  
+
+if st.button("확인"):   
+    client = OpenAI(api_key=f"{api}")
+    
 st.divider()
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
 
 if st.button("실행하기"):
-    openai.api_key = "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"
     assistant = openai.Assistant.create(
         instructions="You are a helpful assistant.",
         model='gpt-4o'
