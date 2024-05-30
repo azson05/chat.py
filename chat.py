e79fb2c4d29f894537c473c19f515477eaec8766
azson05 <songywkd@gmail.com>
2024-05-30 16:23:37 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -6,24 +6,20 @@ st.image("https://docs.streamlit.io/logo.svg")
 st.header("OpenAI API Key를 적어주세요.")
 api = st.text_input("API Key?")
 
-if 'questions' not in st.session_state:
-    st.session_state.questions = []
-
 if st.button("확인"):
   client = OpenAI(api_key=f"{api}")
-  assistant = client.beta.assistants.create(
-    instructions = "You are a helpful assistant.",
-    model='gpt-4o'
-  )
-  
+
 st.divider()
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
 
 if st.button("실행하기"):
-  st.session_state.questions.append(prompt)
-    st.success('질문이 저장되었습니다!')
+  assistant = client.beta.assistants.create(
+    instructions = "You are a helpful assistant.",
+    model='gpt-4o'
+  )
+  
   thread = client.beta.threads.create(
     messages=[
       {
